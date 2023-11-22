b365050fa88e8ddf412424bd77f9a0a3ac25ceea
Jason King-Lowe <NekkidBearGaming@gmail.com>
2023-11-22 17:03:41 -0600

    refactoring Main and adding a settings file for simple configuration.

--- main.py
+++ main.py
@@ -5,7 +5,56 @@ import ctypes
 import pygame
 import sys
 
+def main():
+    pygame.init()
 
-class Game:
-    def __init__(self):
-        pass
+
+
+    # Create the screen
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    # Start the clock
+    clock = pygame.time.Clock()
+
+    while True:
+        # Limit the frame rate to FPS
+        clock.tick(FPS)
+
+        # Process input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                save_data()
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    save_data()
+                    pygame.quit()
+                    sys.exit()
+
+        # Update game state
+        update_game()
+
+        # Draw everything
+        draw(screen)
+
+def save_data():
+    """
+    Save player data and game stats to the database.
+    """
+    pass
+
+def update_game():
+    """
+    Update the game state.
+    """
+    pass
+
+def draw(screen):
+    """
+    Draw everything to the screen.
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
